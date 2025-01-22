a9659fa578b8379ac53a3facae7b9ada20df5c8c
Vicente Rivera <vrivera.dev@gmail.com>
2025-01-22 15:50:03 -0300

    esto lo deberia haber hecho ante slajlda

--- src/components/products/cart/item-preview.tsx
+++ src/components/products/cart/item-preview.tsx
@@ -1,13 +1,13 @@
 import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import { CartProduct } from "@/interfaces/products";
 import { formatPrice } from "@/lib/utils";
-import Image from "next/image";
+import { ImageWrapper } from "../image-wrapper";
 
 export const ItemCartPreview = ({ title, image, price, size }: Partial<CartProduct>) => {
   return (
     <Card className="rounded-none flex gap-2">
-      <Image
-        src={`/products/${image}`}
+      <ImageWrapper
+        src={`${image}`}
         alt="Product 1"
         width={80}
         height={80}

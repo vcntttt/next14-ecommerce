08720c17c26cbd2f07adc8483c7c3d820133c4c6
Vicente Rivera <vrivera.dev@gmail.com>
2025-01-24 15:11:48 -0300

    info

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -3,6 +3,7 @@ import { getPaginatedProducts } from "@/actions/products/pagination";
 import { ProductsGrid } from "@/components/products/grid";
 import { ProductsPagination } from "@/components/products/pagination";
 import { Title } from "@/components/typography/title";
+import { InfoIcon } from "lucide-react";
 export const revalidate = cacheConfig.week;
 
 interface Props {
@@ -19,6 +20,9 @@ export default async function Home({ searchParams }: Props) {
   return (
     <>
       <Title title="Tienda" subtitle="Todos los productos" />
+      <div className="flex items-center gap-2 bg-blue-600 text-white px-6 py-3 rounded-sm">
+        <InfoIcon/> Puedes experimentar demoras en la carga de la página debido a que se esta usando una base de datos gratuita. Gracias por su comprensión.
+      </div>
       <ProductsGrid products={products} />
       <ProductsPagination totalPages={totalPages} />
     </>

f9fb75d99c270e50d510c7ef4a2f56b8dd0e1238
Vicente Rivera <vrivera.dev@gmail.com>
2025-01-29 16:52:12 -0300

    proteccion de `/orders`

--- src/app/(shop)/orders/page.tsx
+++ src/app/(shop)/orders/page.tsx
@@ -2,11 +2,17 @@ import { columns } from "@/components/orders/columns";
 import { DataTable } from "@/components/orders/data-table";
 import { Title } from "../../../components/typography/title";
 import { getOrdersByUser } from "@/actions/orders/get";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
 export default async function OrdersPage() {
   const { orders } = await getOrdersByUser();
-  // console.log("🚀 ~ OrdersPage ~ orders:", orders);
+  const session = await auth();
 
+  if (!session?.user) {
+    return redirect("/");
+  }
+  
   return (
     <div className="container mx-auto py-10">
       <Title title="Mis Ordenes" />

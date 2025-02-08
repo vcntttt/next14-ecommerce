8aebcd8c8882f9feaae8b294e93504453b275cae
Vicente Rivera <vrivera.dev@gmail.com>
2025-02-08 20:30:20 -0300

    fast login responsive

--- src/components/auth/fast-login.tsx
+++ src/components/auth/fast-login.tsx
@@ -6,9 +6,11 @@ import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import Link from "next/link";
 
 export const FastLogin = () => {
   async function handleSignInWithCredentials(data: {
@@ -23,36 +25,44 @@ export const FastLogin = () => {
     }
   }
   return (
-    <Card className="max-w-sm absolute bottom-0 right-5 top-0 h-fit my-auto">
-    <CardHeader>
-      <CardTitle>Acceso rapido</CardTitle>
-      <CardDescription>
-        Accede directamente con cuentas de prueba para explorar la
-        aplicación.
-      </CardDescription>
-    </CardHeader>
-    <CardContent className="flex flex-col gap-2">
-      <Button
-        onClick={() =>
-          handleSignInWithCredentials({
-            email: "admin@shop.com",
-            password: "123456",
-          })
-        }
-      >
-        Ingresar como administrador
-      </Button>
-      <Button
-        onClick={() =>
-          handleSignInWithCredentials({
-            email: "user@shop.com",
-            password: "123456",
-          })
-        }
-      >
-        Ingresar como cliente
-      </Button>
-    </CardContent>
-  </Card>
-  )
-}
+    <Card className="max-w-sm md:absolute md:bottom-0 md:right-5 md:top-0 h-fit my-auto">
+      <CardHeader>
+        <CardTitle>Acceso rapido</CardTitle>
+        <CardDescription>
+          Accede directamente con cuentas de prueba para explorar la aplicación.
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="flex flex-col gap-2">
+        <Button
+          onClick={() =>
+            handleSignInWithCredentials({
+              email: "admin@shop.com",
+              password: "123456",
+            })
+          }
+        >
+          Ingresar como administrador
+        </Button>
+        <Button
+          onClick={() =>
+            handleSignInWithCredentials({
+              email: "user@shop.com",
+              password: "123456",
+            })
+          }
+        >
+          Ingresar como cliente
+        </Button>
+      </CardContent>
+      <CardFooter>
+        <CardDescription>
+          Si tienes tiempo, por favor,{" "}
+          <Link className="underline" href="/auth/register">
+            crea una cuenta
+          </Link>{" "}
+          y prueba la experiencia real de autenticación.
+        </CardDescription>
+      </CardFooter>
+    </Card>
+  );
+};

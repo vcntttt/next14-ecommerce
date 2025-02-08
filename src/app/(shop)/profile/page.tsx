db93a25ce07ca06fad45eabf6bbf88aca6565b17
Vicente Rivera <vrivera.dev@gmail.com>
2025-02-08 20:30:08 -0300

    cerrar sesion y reubicacion de editar direccion

--- src/app/(shop)/profile/page.tsx
+++ src/app/(shop)/profile/page.tsx
@@ -1,6 +1,12 @@
 import { auth } from "@/auth";
 import { Title } from "@/components/typography/title";
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { redirect } from "next/navigation";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
@@ -16,8 +22,16 @@ export default async function ProfilePage() {
       <Title title="Perfil" />
       <Card className="w-full max-w-md">
         <CardHeader className="flex flex-col items-center space-y-4">
-          <Image src={session.user?.image ?? "/person.png"} alt="Avatar" width={100} height={100} className="rounded-full" />
-          <CardTitle className="text-2xl font-bold">{session.user?.name}</CardTitle>
+          <Image
+            src={session.user?.image ?? "/person.png"}
+            alt="Avatar"
+            width={100}
+            height={100}
+            className="rounded-full"
+          />
+          <CardTitle className="text-2xl font-bold">
+            {session.user?.name}
+          </CardTitle>
         </CardHeader>
         <CardContent className="space-y-4">
           <div>
@@ -26,7 +40,9 @@ export default async function ProfilePage() {
           </div>
           <div>
             <p className="text-sm font-medium text-gray-500">Rol</p>
-            <p className="text-lg uppercase">{session.user?.role!.toLowerCase()}</p>
+            <p className="text-lg uppercase">
+              {session.user?.role!.toLowerCase()}
+            </p>
           </div>
           <div>
             <p className="text-sm font-medium text-gray-500">User ID</p>
@@ -35,14 +51,13 @@ export default async function ProfilePage() {
         </CardContent>
         <CardFooter className="flex flex-col gap-y-2">
           <Button asChild className="w-full">
-            <Link href="/orders">
-            Historial de compras
-            </Link>
+            <Link href="/orders">Historial de compras</Link>
           </Button>
           <Button asChild className="w-full">
-            <Link href="/checkout/address/edit">
-            Editar dirección de envío
-            </Link>
+            <Link href="/profile/address">Editar dirección de envío</Link>
+          </Button>
+          <Button asChild className="w-full" variant={"destructive"}>
+            <Link href="/api/auth/signout">Cerrar Sesión</Link>
           </Button>
         </CardFooter>
       </Card>
